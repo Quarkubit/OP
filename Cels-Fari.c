26f636a7ee79892b3b8955a2e808f7aa347db1f9
Quarkubit <AbramovYD+github@gmail.com>
2023-09-30 21:22:58 +0300

    Update Cels-Fari.c

--- Cels-Fari.c
+++ Cels-Fari.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 
 
 int main()
 {
- int f, cels, low, up, step, arr[150];
- low = -4; 
- up = 68; 
- f = low;
+   int f, cels, low, up, arr[150];
+   low = -4; 
+   up = 68; 
+   f = low;
+    /*while (f <= up) {
+       cels = 5 * (f-32) / 9;
+       printf ("%d\t%d\n", f, cels);
+       f = f + step;
+   }*/
 
- /*while (f <= up) {
-    cels = 5 * (f-32) / 9;
-    printf ("%d\t%d\n", f, cels);
-    f = f + step;
- }*/
-
- int i = 1;
- while (f <= up) {
-    cels = 5 * (f-32) / 9;
-    arr[i-1]=f;
-    arr[i]=cels;
-    printf("%d\t%d\n",arr[i-1],arr[i]);
-    i+=2;
-    f+=1;
- }
+   int i = 1;
+   while (f <= up) 
+   {
+       cels = 5 * (f-32) / 9;
+       arr[i-1]=f;
+       arr[i]=cels;
+       printf("%d\t%d\n",arr[i-1],arr[i]);
+       i+=2;
+       f+=1;
+   }
 
 
 
- return 0;
+    return 0;
 }
 
