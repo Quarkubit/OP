5a999d6f1295a4404c9bccf00a4703ac33dd4f69
Quarkubit <AbramovYD+github@gmail.com>
2023-09-30 16:25:35 +0300

    fari->cels
    исправить ошибки

---
+++ Cels-Fari.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <malloc.h>
+
+
+
+int main()
+{
+ int f, cels, low, up, step, arr[150];
+ low = -4; 
+ up = 68; 
+ f = low;
+
+ /*while (f <= up) {
+    cels = 5 * (f-32) / 9;
+    printf ("%d\t%d\n", f, cels);
+    f = f + step;
+ }*/
+
+ int i = 1;
+ while (f <= up) {
+    cels = 5 * (f-32) / 9;
+    arr[i-1]=f;
+    arr[i]=cels;
+    printf("%d\t%d\n",arr[i-1],arr[i]);
+    i+=2;
+    f+=1;
+ }
+
+
+
+ return 0;
+}
+
